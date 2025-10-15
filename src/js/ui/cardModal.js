01ac018d65235c962fd87a52b5ffb78237171529
jaey <jaeyyi98@gmail.com>
2025-10-15 12:18:10 +0900

    preview 1차

--- src/js/ui/cardModal.js
+++ src/js/ui/cardModal.js
@@ -1,17 +1,18 @@
-import {settingCloseModal} from "../utils/openModal.js";
+import { settingCloseModal } from "../utils/openModal.js";
 
 export function createLectureModal(i) {
   const clm = document.createElement("div");
   const lastChar = i.charAt(i.length - 1);
-  console.log(lastChar)
+  console.log(lastChar);
   const name = JSON.parse(localStorage.getItem("courses"));
   let dd;
   name.forEach((course) => {
-    console.log(course.id)
+    console.log(course.id);
     if (course.id == lastChar) {
       dd = course;
     }
-  })
+  });
+  console.log(name);
   clm.innerHTML = `
 <div id="${i}" class="modal">
   <div class="modal-content">
@@ -23,14 +24,34 @@ export function createLectureModal(i) {
       <input type="hidden" id="course">
       <div>
       <div class="form-group">
+        <label for="thumbnail">썸네일 이미지</label>
+        <img id = "thumbnail-notice" scr = "" alt="썸네일 이미지" style= style="position: absolute; right: 20px; top:0.75rem; color: black;">
+      </div>
+
+
+
+      <div class="form-group">
         <label for="lecture-name">강의명</label>
-        <input type="text" id="lecture-name" class="form-input" placeholder="강의명을 입력해주세요." required>
+        <input type="text" id="lecture-name" class="form-input" value="${name}." required>
+      </div>
+      <div class="form-group">
+        <label for="category">카테고리</label>
+        <input type="text" id="category"" class="form-input" placeholder="카테고리명을 입력하세요." required>
+      </div>
+      <div class="form-group">
+        <label for="lecture-name">난이도</label>
+        <input type="text" name="level" id="level" class="form-input" placeholder="난이도" required>
+      </div>
+        <div class="form-group">
+        <label for="lecture-name">강의 소개</label>
+        <input type="text" id="introduce" class="form-input" placeholder="설명 글에 소개할 글을 간단하게 작성해주세요." required>
       </div>
       </div>
       
       <div class="modal-actions">
         <button type="button" class="cancel-btn">취소</button>
         <button type="submit" class="save-btn">저장</button>
+        <button type="submit" class="edit-btn">수정</button>
       </div>
     </form>
   </div>
@@ -41,4 +62,3 @@ export function createLectureModal(i) {
   const modalId = document.getElementById(i);
   settingCloseModal(modalId);
 }
-
--- src/js/ui/createLectureModal/createLectureModal.js
+++ src/js/ui/createLectureModal/createLectureModal.js
@@ -1,16 +1,21 @@
-import {openFileInput} from "../../utils/file/openFileInput.js";
-import {fileInsert} from "../../utils/file/fileInsert.js";
-import {cardTemplate} from "../card/card.js";
+import { openFileInput } from "../../utils/file/openFileInput.js";
+import { fileInsert } from "../../utils/file/fileInsert.js";
+import { cardTemplate } from "../card/card.js";
 
-document.getElementsByClassName("modals").item(0).appendChild(await createLectureModal());
+document
+  .getElementsByClassName("modals")
+  .item(0)
+  .appendChild(await createLectureModal());
 
 async function createLectureModal() {
-  const res = await fetch('/src/js/ui/createLectureModal/createLectureModalTemplate.html');
+  const res = await fetch(
+    "/src/js/ui/createLectureModal/createLectureModalTemplate.html"
+  );
   const htmlText = await res.text();
 
-  const doc = new DOMParser().parseFromString(htmlText, 'text/html');
+  const doc = new DOMParser().parseFromString(htmlText, "text/html");
 
-  return doc.querySelector('#createLectureModal');
+  return doc.querySelector("#createLectureModal");
 }
 
 const noticeBtn = document.getElementById("thumbnail-notice");
@@ -20,10 +25,10 @@ const modal = document.getElementById("createLectureModal");
 const form = modal.querySelector("#create-form");
 
 // 파일 클릭
-openFileInput(noticeBtn, fileInput)
+openFileInput(noticeBtn, fileInput);
 
 // 파일 넣는 함수
-fileInsert(noticeBtn, fileInput, preview)
+fileInsert(noticeBtn, fileInput, preview);
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault(); // 기본 새로고침 방지
@@ -34,7 +39,7 @@ form.addEventListener("submit", async (e) => {
   const introduce = document.getElementById("introduce").value.trim();
   const thumbnail = document.getElementById("previewImage").src;
 
-  console.log(localStorage.getItem("courses"))
+  console.log(localStorage.getItem("courses"));
   const id = JSON.parse(localStorage.getItem("courses")).length ?? 0;
 
   if (!lectureName || !category || !level || !introduce) {
@@ -48,7 +53,12 @@ form.addEventListener("submit", async (e) => {
   }
 
   const newCourse = {
-    lectureName, introduce, level, category, thumbnail, id
+    lectureName,
+    introduce,
+    level,
+    category,
+    thumbnail,
+    id,
   };
 
   const storedCourses = JSON.parse(localStorage.getItem("courses")) || [];
@@ -57,7 +67,7 @@ form.addEventListener("submit", async (e) => {
 
   alert("강좌가 저장되었습니다!");
   localStorage.removeItem("thumbnailImage");
-  modal.style.display = 'none';
+  modal.style.display = "none";
   form.reset();
   const card = await cardTemplate(newCourse, storedCourses.length - 1);
   document.getElementById("card-container").appendChild(card);
